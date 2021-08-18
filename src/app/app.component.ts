5eb9072e6bf501f79017dd5e73387d23d5128efe
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-08-18 14:49:37 -0700

    spacing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,26 +19,26 @@ export class AppComponent {
             {
                 label: 'PDMSS',
                 'routerLink': ['table'],
-                items: [{
-                    label: 'New',
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        { label: 'Project' },
-                        { label: 'Other' },
-                    ]
-                },
-                { label: 'Open' },
-                { label: 'Quit' }
-                ]
+                // items: [{
+                //     label: 'New',
+                //     icon: 'pi pi-fw pi-plus',
+                //     items: [
+                //         { label: 'Project' },
+                //         { label: 'Other' },
+                //     ]
+                // },
+                // { label: 'Open' },
+                // { label: 'Quit' }
+                // ]
             },
             {
                 label: 'Home',
                 icon: 'pi pi-fw pi-home',
                 'routerLink': ['/'],
-                items: [
-                    { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-                    { label: 'Refresh', icon: 'pi pi-fw pi-refresh', 'routerLink': ['/dashboard'], }
-                ]
+                // items: [
+                //     { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+                //     { label: 'Refresh', icon: 'pi pi-fw pi-refresh', 'routerLink': ['/dashboard'], }
+                // ]
             },
             {
                 label: 'ITE',
